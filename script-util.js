930fd09df18066ed8fb737caab39a0d0aa56f96f
Paulo Henrique Marchi <pamarchi@uolinc.com>
2022-11-04 11:56:47 -0300

    feat(dream_team): paulo marchi description + function get age

---
+++ script-util.js
@@ -0,0 +1,15 @@
+
+
+//função para retornar idade a partir da data de nascimento
+function getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return document.write(age);
+}
