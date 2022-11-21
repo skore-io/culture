052dfb87905ad35e8218f6634245c5e1e2b4b39a
Renato Augusto <orenato.ames@outlook.com>
2022-11-21 08:18:19 -0300

    docs(dream_team): add tais hamamoto profile

--- script-util.js
+++ script-util.js
@@ -1,15 +1,11 @@
-
-
-//função para retornar idade a partir da data de nascimento
+// função para retornar idade a partir da data de nascimento
 function getAge(dateString) {
-    const today = new Date();
-    const birthDate = new Date(dateString);
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
+  const today = new Date()
+  const birthDate = new Date(dateString)
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const m = today.getMonth() - birthDate.getMonth()
 
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) age--
 
-    return document.write(age);
+  return document.write(age)
 }
