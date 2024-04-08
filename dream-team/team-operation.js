49a34a13a7b7d75a0bdea5dea5d6ba10fe4b4d3e
gabiekg <43047145+gabiekg@users.noreply.github.com>
2024-04-08 14:53:38 -0300

    docs(dream_team): add larson's profile"

--- dream-team/team-operation.js
+++ dream-team/team-operation.js
@@ -16,7 +16,8 @@ const operationTeam = [
   {
     name: "Larson Iago Silva",
     role: operationRoles.support,
-    description: "Lorem Ipsum...",
+    description: "Sou o Larson Guimarães, tenho getTotalYears('28/03/2001') anos e sou de São Paulo. Tenho um amor genuíno por carros (maior paixão da vida), tecnologia, comunicação, viagens e por gastronomia. 
+      Minha vida pessoal e minha carreira foram estimuladas pela junção dessas paixões. Sou formado em Marketing e fiz 3 semestres de Publicidade (não posso jogar fora 1 ano e meio de sofrimento hahahah).",
     social: "https://www.linkedin.com/in/larson-guimaraes",
     avatar: "https://media.licdn.com/dms/image/C4E03AQGRmexO86JMXw/profile-displayphoto-shrink_800_800/0/1645549887307?e=1717632000&v=beta&t=tepxaTKw3aVybCMXqfTmZ4y-_Q6Sg15UhK9cJXz4O3M"
   },
