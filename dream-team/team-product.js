63b0ac7eeaf2d37b87ec8657ec84ff482ada304c
Renato Augusto <orenato.ames@outlook.com>
2025-01-22 15:45:27 -0400

    feat: add UX team

--- dream-team/team-product.js
+++ dream-team/team-product.js
@@ -1,8 +1,6 @@
 const productRoles = {
   gpm: 'Group Product Manager',
   pm: 'Product Manager',
-  ux: 'UX',
-  ux_lead: 'UX Lead',
 }
 
 const productTeam = [
@@ -16,17 +14,6 @@ const productTeam = [
       Tenho muito mais hobbies do que tempo, e por isso cada hora estou priorizando um diferente, mas curto muito assistir futebol americano, tocar violão, games, muita série e principalmente viajar com a família.`,
     linkedin: 'https://www.linkedin.com/in/fabriziotassitano',
     avatar: 'https://i.imgur.com/OeaF3Tq.png',
-  },
-  {
-    name: 'Hendrik Funari',
-    role: productRoles.ux_lead,
-    description: `
-      Fala, pessoal! Sou o Hendrik, mas podem me chamar como preferirem — já ouvi de tudo, desde ‘Endrique’ até ‘Rendrick’, então fiquem à vontade!
-      Sou mineiro de nascença e sempre vivi em Minas Gerais; hoje moro em Uberlândia, cidade pela qual tenho um grande carinho.
-      Me formei em Sistemas de Informação pela UFU, mas foi no design que encontrei minha verdadeira paixão, onde já atuo há mais de getTotalYears('01/01/2018') anos.
-      Além disso, sou fã de viagens e corrida, e um dos meus maiores sonhos é participar das grandes maratonas pelo mundo!`,
-    linkedin: 'https://www.linkedin.com/in/rendrike',
-    avatar: 'https://i.imgur.com/5JeOq3X.png',
   },
   {
     name: 'Thiago Cassimiro',
@@ -62,14 +49,5 @@ const productTeam = [
       'Oiee, meu nome é Luíza, mas quase todo mundo me chama de Lu. Sou mineira e amo bastante meu estado, mas também sou fascinada por viagens. Qualquer brecha que tenho estou na estrada! Sou bem curiosa e bem desinquieta, me formei em engenharia de controle e automação, fiz mestrado em engenharia elétrica, fui professora, desenvolvedora e pesquisadora, e depois disso vi que o que realmente gosto é de resolver problemas. Migrei para a gestão de projetos e agora estou me aventurando no mundo de produto :)',
     linkedin: 'https://www.linkedin.com/in/luizasernizonguimaraes',
     avatar: 'https://i.imgur.com/5EfpNty.png',
-  },
-  {
-    name: 'Gustavo Dinelli',
-    role: productRoles.ux,
-    description: `Olá, meu nome é Gustavo, mas muitos me conhecem pelo sobrenome Dinelli. Formado em design gráfico e pós-graduado em UX. Com os avanços da tecnologia, fiz a transição do impresso e produtos físicos para o mundo digital.
-      Ao longo da minha carreira, tive a oportunidade de trabalhar com marcas renomadas como Disney, Marvel, Turma da Mônica, Globo, FIFA e diversos times de futebol. Atualmente, na Uol EdTech, já passei por todas as plataformas da empresa e participei de várias iniciativas, incluindo layout para PowerBI, criação de materiais de conteúdo, estudo de cores e desenvolvimento de logotipos para o marketing.
-      No meu tempo livre, sou um colecionador de moedas, cédulas, action figures e vinis (os famosos LPs). Meus hobbies incluem ouvir música (Rock, Blues, Jazz, Reggae, Indie Rock, Música Clássica e Eletrônica), assistir corridas como MotoGP, F1 e Turismo, além de jogar gêneros como gerenciamento de cidades, fps, corrida ou RPGs.`,
-    linkedin: 'https://www.linkedin.com/in/gustavodinelli',
-    avatar: 'https://i.imgur.com/OEru0kL.png',
   },
 ]
---
+++ dream-team/team-ux.js
@@ -0,0 +1,27 @@
+const uxRoles = {
+  ux_lead: 'UX Lead',
+  ux: 'UX',
+}
+
+const uxTeam = [
+  {
+    name: 'Hendrik Funari',
+    role: uxRoles.ux_lead,
+    description: `
+      Fala, pessoal! Sou o Hendrik, mas podem me chamar como preferirem — já ouvi de tudo, desde ‘Endrique’ até ‘Rendrick’, então fiquem à vontade!
+      Sou mineiro de nascença e sempre vivi em Minas Gerais; hoje moro em Uberlândia, cidade pela qual tenho um grande carinho.
+      Me formei em Sistemas de Informação pela UFU, mas foi no design que encontrei minha verdadeira paixão, onde já atuo há mais de getTotalYears('01/01/2018') anos.
+      Além disso, sou fã de viagens e corrida, e um dos meus maiores sonhos é participar das grandes maratonas pelo mundo!`,
+    linkedin: 'https://www.linkedin.com/in/rendrike',
+    avatar: 'https://i.imgur.com/5JeOq3X.png',
+  },
+  {
+    name: 'Gustavo Dinelli',
+    role: uxRoles.ux,
+    description: `Olá, meu nome é Gustavo, mas muitos me conhecem pelo sobrenome Dinelli. Formado em design gráfico e pós-graduado em UX. Com os avanços da tecnologia, fiz a transição do impresso e produtos físicos para o mundo digital.
+      Ao longo da minha carreira, tive a oportunidade de trabalhar com marcas renomadas como Disney, Marvel, Turma da Mônica, Globo, FIFA e diversos times de futebol. Atualmente, na Uol EdTech, já passei por todas as plataformas da empresa e participei de várias iniciativas, incluindo layout para PowerBI, criação de materiais de conteúdo, estudo de cores e desenvolvimento de logotipos para o marketing.
+      No meu tempo livre, sou um colecionador de moedas, cédulas, action figures e vinis (os famosos LPs). Meus hobbies incluem ouvir música (Rock, Blues, Jazz, Reggae, Indie Rock, Música Clássica e Eletrônica), assistir corridas como MotoGP, F1 e Turismo, além de jogar gêneros como gerenciamento de cidades, fps, corrida ou RPGs.`,
+    linkedin: 'https://www.linkedin.com/in/gustavodinelli',
+    avatar: 'https://i.imgur.com/OEru0kL.png',
+  },
+]
